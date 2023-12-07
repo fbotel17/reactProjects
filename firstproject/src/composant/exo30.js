85a0a064c2a54c9cd23f7c799bee2b1b3a57ab92
fbotel17 <faustbtl17@gmail.com>
2023-12-07 14:52:08 +0100

    exo 30

---
+++ firstproject/src/composant/exo30.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function Exo30(){
+    const login="Fau";
+    const password="password";
+    const logins=["fau", "tib", "mat", "tom", "edou", "dam", "theo", "jean", "pas hugo", "surtout pas hugo", "vraiment pas hugo"];
+
+
+
+    const [isConnected, setIsConnected] = useState(false);
+
+    const toggleConnected = () => {
+        setIsConnected(!isConnected);
+    };
+
+    return(
+        <div>
+            <button onClick={toggleConnected}>
+                {isConnected ? 'Passer à fau' : 'Passer à vrai'}
+            </button>
+            {isConnected ? (
+                <div>vrai</div>
+            ) :
+            (<div>
+                <ul>
+                    {logins.map(login => (
+                        <li>{login}</li>
+                    ))}
+                </ul>
+            </div>)
+            }
+        </div>
+    );
+};
+
+export default Exo30;
