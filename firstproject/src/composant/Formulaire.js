b0330e9d221a5037789f684a88d6d397e92810b0
fbotel17 <faustbtl17@gmail.com>
2023-12-07 15:18:14 +0100

    exo 31

--- firstproject/src/composant/Formulaire.js
+++ firstproject/src/composant/Formulaire.js
@@ -1,34 +1,54 @@
 import React, { useState } from 'react';
 
 function Formulaire(){
-    const login="Fau";
-    const password="password";
-    const logins=["fau", "tib", "mat", "tom", "edou", "dam", "theo", "jean", "pas hugo", "surtout pas hugo", "vraiment pas hugo"];
 
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [color, setColor] = useState('black'); // Nouvel état pour la couleur
 
+    // Fonction pour gérer la soumission du formulaire
+    const handleSubmit = (form) => {
+        form.preventDefault();
 
-    const [isConnected, setIsConnected] = useState(false);
-
-    const toggleConnected = () => {
-        setIsConnected(!isConnected);
+        // Vérifier si le mot de passe est correct (1234)
+        if (password === '1234') {
+            setColor('green'); // Si le mot de passe est correct, la couleur est verte
+        } else if (password === '') {
+            setColor('black'); // Si rien n'a été entré, la couleur est noire
+        } else {
+            setColor('red'); // Si le mot de passe est incorrect, la couleur est rouge
+        }
     };
 
-    return(
-        <div>
-            <button onClick={toggleConnected}>
-                {isConnected ? 'Passer à fau' : 'Passer à vrai'}
-            </button>
-            {isConnected ? (
-                <div>vrai</div>
-            ) :
-            (<div>
-                <ul>
-                    {logins.map(login => (
-                        <li>{login}</li>
-                    ))}
-                </ul>
-            </div>)
-            }
+
+
+    return (
+        <div style={{ color }}>
+            <h2>Formulaire</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Login:
+                    <input
+                        type="text"
+                        value={login}
+                        onChange={(form) => setLogin(form.target.value)}
+                    />
+                </label>
+                <div></div>
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(form) => setPassword(form.target.value)}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+            {color === 'red' && <p>Mot de passe incorrect, veuillez réessayer.</p>}
+
+            {color === 'green' && <p>Vous etes connecté</p>}
         </div>
     );
 };
